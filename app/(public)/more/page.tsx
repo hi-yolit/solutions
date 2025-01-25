296e7f28580db082345caf5e4b7d8c436cd46fa5
Kgotso Phiri <kgotsokg12@gmail.com>
2025-01-25 16:11:13 +0200

    feat: add more page

---
+++ app/(public)/more/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const More = () => {
+  return (
+    <div className='py-16'>
+      <p>Account Information w/ selected plan</p>
+      <p>Personalised card with #Number</p>
+      <p>How to videos</p>
+      <p>Other stuff from YAAS</p>
+    </div>
+  );
+}
+
+export default More
