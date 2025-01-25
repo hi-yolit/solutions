ffc254398e63a25a39801b0d9752afc65d3b2e5f
Kgotso Phiri <kgotsokg12@gmail.com>
2025-01-25 16:15:20 +0200

    chore: more text

--- app/(public)/more/page.tsx
+++ app/(public)/more/page.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const More = () => {
   return (
-    <div className='py-16'>
+    <div className="py-16">
       <p>Account Information w/ selected plan</p>
+      <p>On current plan -> show switch plan or cancel subscription</p>
       <p>Personalised card with #Number</p>
       <p>How to videos</p>
       <p>Other stuff from YAAS</p>
