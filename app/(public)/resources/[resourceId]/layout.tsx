a9b405806071d331f118423a7d16fddf9ec03be2
Kgotso Phiri <kgotsokg12@gmail.com>
2025-02-07 19:27:05 +0200

    feat: make the arrow go back

--- app/(public)/resources/[resourceId]/layout.tsx
+++ app/(public)/resources/[resourceId]/layout.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 
 const ResourceLayout = ({ children }: { children: React.ReactNode }) => {
     return (
-    <section>
-      <div className="w-full py-4">
-        <ArrowLeft />
-      </div>
-      {children}
-    </section>
-  );
+      <section>
+        <div className="w-full py-4 cursor-pointer">
+          <Link href="/home">
+            <ArrowLeft />
+          </Link>
+        </div>
+        {children}
+      </section>
+    );
 };
 
 export default ResourceLayout;
