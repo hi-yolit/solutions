1d41bcd99912a591125c8b7b90673dd49dce7c45
Tapiwa Kambanga <kambangacraig@gmail.com>
2025-04-18 21:05:16 +0200

    layout

--- features/exercise/components/exercise-content.tsx
+++ features/exercise/components/exercise-content.tsx
@@ -472,7 +472,7 @@ const ExerciseContent = ({ question }: { question: any }) => {
   }
 
   return (
-    <div className="max-w-4xl mx-auto p-2 space-y-6">
+    <div className="mx-auto p-2 space-y-6">
       {/* Credit error display if any */}
       {creditError && (
         <Alert color="red" title="Error" className="mb-4">
